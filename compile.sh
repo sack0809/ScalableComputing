75e9360d2daac8ccaea478dc6d4c1897620d73bb
Saquib08 <er.saquib08@gmail.com>
2017-11-24 17:58:03 +0000

    Adding scripts for Compilation of the java files

---
+++ compile.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+####THIS SCRIPT IS USED FOR COMPILATION OF JAVA FILES##
+pwd
+javac Server.java
+javac ClientThread.java
+javac testChatRoom.java
+
+echo "All files has been compiled"
