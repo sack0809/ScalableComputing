42a5322993139d1dd3a6ef95e44395e7ec12385b
Saquib08 <er.saquib08@gmail.com>
2017-12-02 02:18:02 +0000

    Getting file names per commit

--- Assignment2/CloneRemoteRepository.java
+++ Assignment2/CloneRemoteRepository.java
@@ -1,94 +1,147 @@
 
 
+/*
+   Copyright 2013, 2014 Dominik Stadler
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import org.eclipse.jgit.api.Git;
-
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.errors.CorruptObjectException;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectLoader;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevSort;
 import org.eclipse.jgit.revwalk.RevTree;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.treewalk.CanonicalTreeParser;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.eclipse.jgit.treewalk.filter.PathFilter;
 
-
-
+/**
+ * Snippet which shows how to use RevWalk and TreeWalk to read the contents
+ * of a specific file from a specific commit.
+ *
+ * @author dominik.stadler at gmx.at
+ */
 public class CloneRemoteRepository {
+  
+	private static final String REMOTE_URL = "https://github.com/sack0809/ScalableComputing.git";
+	
+	
+    public static void main(String[] args) throws IOException , GitAPIException{
+    	
+    	 try (Git result = Git.cloneRepository()
+                 .setURI(REMOTE_URL)
+                 .setDirectory(new File("/Users/playsafe/Desktop/Java/Assignment2/src/repo"))
+                  .setCloneAllBranches(true)
+                  
+                 .call()) {
+ 	        // Note: the call() returns an opened repository already which needs to be closed to avoid file handle leaks!
+ 	        System.out.println("Having repository: " + result.getRepository().getDirectory());
+ 	        
+         }
+    	
+    	
+    	
+    	FileRepositoryBuilder builder = new FileRepositoryBuilder();
+        Repository repository = builder
+                .setGitDir(new File("/Users/playsafe/Desktop/Java/Assignment2/src/repo/.git")).readEnvironment()
+                .findGitDir().build();
 
-    private static final String REMOTE_URL = "https://github.com/sack0809/ScalableComputing.git";
+        listRepositoryContents(repository);
 
-    public static void main(String[] args) throws IOException, GitAPIException {
-       /* // prepare a new folder for the cloned repository
-        File localPath = File.createTempFile("/Users/playsafe/Desktop/Java/Assignment2/","");
-     //File localPath = new File("/Users/playsafe/Desktop/Java/");
-        if(!localPath.delete()) {
-            throw new IOException("Could not delete temporary file " + localPath);
-        }
+        repository.close();
+    	
+    	}
 
-        // then clone
-        System.out.println("Cloning from " + REMOTE_URL + " to " + localPath);*/
-        try (Git result = Git.cloneRepository()
-                .setURI(REMOTE_URL)
-                .setDirectory(new File("/Users/playsafe/Desktop/Java/Assignment2/src/repo"))
-                 .setCloneAllBranches(true)
-                 
-                .call()) {
-	        // Note: the call() returns an opened repository already which needs to be closed to avoid file handle leaks!
-	        System.out.println("Having repository: " + result.getRepository().getDirectory());
-	        
-        }
-	        Git.open(new File("//Users/playsafe/Desktop/Java/Assignment2/src/repo/.git"))
-	        .checkout();
-	        Repository repo = new FileRepository("//Users/playsafe/Desktop/Java/Assignment2/src/repo/.git");
-	        Git git = new Git(repo);
-	        RevWalk walk = new RevWalk(repo);
+	@SuppressWarnings("deprecation")
+	private static void listRepositoryContents(Repository repository) throws MissingObjectException, IncorrectObjectTypeException, CorruptObjectException, IOException, GitAPIException {
+		// TODO Auto-generated method stub
+		@SuppressWarnings("deprecation")
+		Git git = new Git(repository);
+		Ref head = repository.getRef("HEAD");
 
-	        List<Ref> branches = git.branchList().call();
-	        for (Ref branch : branches) {
-	            String branchName = branch.getName();
+        // a RevWalk allows to walk over commits based on some filtering that is defined
+        RevWalk walk = new RevWalk(repository);
 
-	            System.out.println("Commits of branch: " + branch.getName());
-	            System.out.println("-------------------------------------");
+        
+        List<Ref> branches = git.branchList().call();
+        for (Ref branch : branches) {
+            String branchName = branch.getName();
 
-	            Iterable<RevCommit> commits = git.log().all().call();
+            System.out.println("Commits of branch: " + branch.getName());
+            System.out.println("-------------------------------------");
 
-	            for (RevCommit commit : commits) {
-	                boolean foundInThisBranch = false;
+            Iterable<RevCommit> commits = git.log().all().call();
 
-	                RevCommit targetCommit = walk.parseCommit(repo.resolve(
-	                        commit.getName()));
-	                for (Map.Entry<String, Ref> e : repo.getAllRefs().entrySet()) {
-	                    if (e.getKey().startsWith(Constants.R_HEADS)) {
-	                        if (walk.isMergedInto(targetCommit, walk.parseCommit(
-	                                e.getValue().getObjectId()))) {
-	                            String foundInBranch = e.getValue().getName();
-	                            if (branchName.equals(foundInBranch)) {
-	                                foundInThisBranch = true;
-	                                break;
-	                            }
-	                        }
-	                    }
-	                }
+            for (RevCommit commit : commits) {
+                boolean foundInThisBranch = false;
 
-	                if (foundInThisBranch) {
-	                    System.out.println(commit.getName());
-	                    System.out.println(commit.getAuthorIdent().getName());
-	                    System.out.println(new Date(commit.getCommitTime()));
-	                    System.out.println(commit.getFullMessage());
-	                }
-	            }
-	       
+                RevCommit targetCommit = walk.parseCommit(repository.resolve(
+                        commit.getName()));
+                
+                for (Map.Entry<String, Ref> e : repository.getAllRefs().entrySet()) {
+                    if (e.getKey().startsWith(Constants.R_HEADS)) {
+                        if (walk.isMergedInto(targetCommit, walk.parseCommit(
+                                e.getValue().getObjectId()))) {
+                            String foundInBranch = e.getValue().getName();
+                            if (branchName.equals(foundInBranch)) {
+                                foundInThisBranch = true;
+                                RevTree tree = targetCommit.getTree();
+                                TreeWalk treeWalk = new TreeWalk(repository);
+                                treeWalk.addTree(tree);
+                                treeWalk.setRecursive(false);
+                                while (treeWalk.next()) {
+                                    System.out.println("found: " + treeWalk.getPathString());
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
 
-	        
-	        
-        }
+                if (foundInThisBranch) {
+                    System.out.println(commit.getName());
+                    //System.out.println(commit.getAuthorIdent().getName());
+                    //System.out.println(new Date(commit.getCommitTime()));
+                    //System.out.println(commit.getFullMessage());
+                }
+            }
+       
+
+        
+        
+    
+        
+       
     }
+	}
+    
 }
+        
